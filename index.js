d84e3e74f94745dd121b18d089d469350d330a93
damjandado <damjanspace@gmail.com>
2017-11-29 16:41:01 +0100

    auth rc

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
-/*const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
-require('./models/Survey');
+// require('./models/Survey');
 require('./services/passport');
 
-// mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);*/
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-/*app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -25,8 +25,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
-require('./routes/surveyRoutes')(app);*/
+// require('./routes/billingRoutes')(app);
+// require('./routes/surveyRoutes')(app);
 
 /*if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
